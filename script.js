288f78d2f39ffa240890cbe7396193de6abdb4cc
iKenshu <undefined.hlo.o@gmail.com>
2015-05-10 13:35:33 -0430

    Subiendo puls4

---
+++ script.js
@@ -0,0 +1,13 @@
+var $form = $('#formulario'),
+	$titulo = $('#titulo'),
+	$url = $('#url'),
+	$button = $('#mostrar-form'),
+	$list = $('#contenido'),
+	$post = $('.item').first();
+
+function mostrarFormulario(){
+	$form.slideToggle();
+}
+
+// Eventos
+$button.click( mostrarFormulario );
